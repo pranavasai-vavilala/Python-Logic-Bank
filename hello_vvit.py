1de7eb49955f5a2352850e71567b1f8e413472a8
Pranavasai Vavilala <pranavasaivavilala1@gmail.com>
2026-03-27 09:58:48 +0530

    Create hello_vvit.py
    Initial commit

---
+++ hello_vvit.py
@@ -0,0 +1,7 @@
+# My first GitHub script
+name = "Pranavasai"
+college = "Vasireddy Venkatadri Institute of Technology"
+year = "2025-2029"
+
+print(f"Hi, I am {name}. I'm a CS student at {college}, Batch {year}.")
+print("I am officially starting my Software Engineering journey today!")
